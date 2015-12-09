59abd0a8a8268fb4107230c3943a5309c9d0379f
VladislavZhukovsky <jaymzmanunited@gmail.com>
2015-12-09 03:52:17 +0300

    Task - IQueryable - Part 1 & 2.

---
+++ source/03 - Expressions and IQueryable/IQueryable/E3SProviderTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sample03.E3SClient.Entities;
+using Sample03.E3SClient;
+using System.Configuration;
+using System.Linq;
+
+namespace Sample03
+{
+    [TestClass]
+    public class E3SProviderTests
+    {
+        [TestMethod]
+        public void TaskPart1()
+        {
+            //Changes are in class ExpressionToFTSRequestTranslator
+            var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            Console.WriteLine("");
+            foreach (var emp in employees.Where(e => "EPBYMINW4226" == e.workstation ))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+            }
+        }
+
+        [TestMethod]
+        public void TaskPart2StartsWith()
+        {
+            //Changes are in class ExpressionToFTSRequestTranslator
+            var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            Console.WriteLine("StartsWith");
+            foreach (var emp in employees.Where(e => e.workstation.StartsWith("EPBYMINW422")))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+            }
+        }
+
+        [TestMethod]
+        public void TaskPart2EndsWith()
+        {
+            //Changes are in class ExpressionToFTSRequestTranslator
+            var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            Console.WriteLine("EndsWith");
+            foreach (var emp in employees.Where(e => e.workstation.EndsWith("226")))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+            }
+        }
+
+        [TestMethod]
+        public void TaskPart2Contains()
+        {
+            //Changes are in class ExpressionToFTSRequestTranslator
+            var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            Console.WriteLine("Contains");
+            foreach (var emp in employees.Where(e => e.workstation.Contains("PBYMINW422")))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+            }
+        }
+    }
+}
---
+++ source/03 - Expressions and IQueryable/IQueryable/ExpressionToFTSRequestTranslator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample03
+{
+    public class ExpressionToFTSRequestTranslator : ExpressionVisitor
+    {
+        StringBuilder resultString;
+
+        public string Translate(Expression exp)
+        {
+            resultString = new StringBuilder();
+            Visit(exp);
+
+            return resultString.ToString();
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.DeclaringType == typeof(Queryable))
+            {
+                switch (node.Method.Name)
+                {
+                    case "Where":
+                        var predicate = node.Arguments[1];
+                        Visit(predicate);
+                        return node;
+                }
+            }
+            //TASK - PART 2
+            if (node.Method.DeclaringType == typeof(string))
+            {
+                Visit(node.Object);
+                resultString.Append("(");
+                switch (node.Method.Name)
+                {
+                    case "StartsWith":
+                        var constant = node.Arguments[0];
+                        Visit(constant);
+                        resultString.Append("*");
+                        break;
+                    case "EndsWith":
+                        resultString.Append("*");
+                        constant = node.Arguments[0];
+                        Visit(constant);
+                        break;
+                    case "Contains":
+                        resultString.Append("*");
+                        constant = node.Arguments[0];
+                        Visit(constant);
+                        resultString.Append("*");
+                        break;
+                    default:
+                        throw new NotSupportedException("Not supported operation");
+                }
+                resultString.Append(")");
+                return node;
+            }
+            return base.VisitMethodCall(node);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            switch (node.NodeType)
+            {
+                case ExpressionType.Equal:
+                    //TASK - PART 1
+                    if (node.Left.NodeType == ExpressionType.Constant)
+                    {
+                        Visit(node.Right);
+                        resultString.Append("(");
+                        Visit(node.Left);
+                    }
+                    else
+                    {
+                        Visit(node.Left);
+                        resultString.Append("(");
+                        Visit(node.Right);
+                    }
+                    resultString.Append(")");
+                    break;
+                default:
+                    throw new NotSupportedException(string.Format("Operation {0} is not supported", node.NodeType));
+            };
+
+            return node;
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            resultString.Append(node.Member.Name).Append(":");
+
+            return base.VisitMember(node);
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            resultString.Append(node.Value);
+
+            return node;
+        }
+    }
+}
