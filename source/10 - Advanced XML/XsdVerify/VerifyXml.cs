03c8e6eb5dc6648957386becbb5636e8012a8341
Vladislav Zhukouski <Vladislav_Zhukouski@epam.com>
2016-02-25 19:09:14 +0300

    Advanced XML - XSD task done

---
+++ source/10 - Advanced XML/XsdVerify/VerifyXml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XsdVerify
+{
+    [TestClass]
+    public class VerifyXml
+    {
+        XmlReaderSettings settings;
+
+        public void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            settings = new XmlReaderSettings();
+            settings.Schemas.Add("http://library.by/catalog", "books.xsd");
+            settings.ValidationType = ValidationType.Schema;
+        }
+
+        [TestMethod]
+        public void CheckValidXML()
+        {
+            try
+            {
+                XmlReader reader = XmlReader.Create("books.xml", settings);
+                XmlDocument document = new XmlDocument();
+                document.Load(reader);
+                ValidationEventHandler eventHandler = new ValidationEventHandler(ValidationEventHandler);
+                document.Validate(eventHandler);
+                reader.Close();
+            }
+            catch (XmlSchemaValidationException ex)
+            {
+                Console.WriteLine(String.Format("{0} \nLine: {1}, position: {2}", ex.Message, ex.LineNumber, ex.LinePosition));
+            }
+        }
+
+        [TestMethod]
+        public void CheckInvalidXML1()
+        {
+            try
+            {
+                XmlReader reader = XmlReader.Create("books_invalid1.xml", settings);
+                XmlDocument document = new XmlDocument();
+                document.Load(reader);
+                ValidationEventHandler eventHandler = new ValidationEventHandler(ValidationEventHandler);
+                document.Validate(eventHandler);
+                reader.Close();
+            }
+            catch (XmlSchemaValidationException ex)
+            {
+                Console.WriteLine(String.Format("{0} \nLine: {1}, position: {2}", ex.Message, ex.LineNumber, ex.LinePosition));
+            }
+        }
+
+        [TestMethod]
+        public void CheckInvalidXML2()
+        {
+            try
+            {
+                XmlReader reader = XmlReader.Create("books_invalid2.xml", settings);
+                XmlDocument document = new XmlDocument();
+                document.Load(reader);
+                ValidationEventHandler eventHandler = new ValidationEventHandler(ValidationEventHandler);
+                document.Validate(eventHandler);
+                reader.Close();
+            }
+            catch (XmlSchemaValidationException ex)
+            {
+                Console.WriteLine(String.Format("{0} \nLine: {1}, position: {2}", ex.Message, ex.LineNumber, ex.LinePosition));
+            }
+        }
+
+    }
+}
